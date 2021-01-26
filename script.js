28f5c72ab821d5c17a8fb31416aeaadaee3cc78b
ymaulana <y.maulana1609@gmail.com>
2021-01-26 23:20:14 +0700

    membuat rest-api menggunakan ajax&jquery dengan metode callback function

---
+++ script.js
@@ -0,0 +1,77 @@
+$(".search-button").on("click", function () {
+  $.ajax({
+    url:
+      "http://www.omdbapi.com/?apikey=c4038750&s=" + $(".input-keyword").val(),
+    success: (results) => {
+      const movies = results.Search;
+      console.log(movies);
+      let cards = "";
+      movies.forEach((m) => {
+        cards += showCards(m);
+      });
+      $(".movie-container").html(cards);
+
+      //ketika tombol detail di click
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url:
+            "http://www.omdbapi.com/?apikey=c4038750&i=" +
+            $(this).data("bs-imdbid"),
+          success: (m) => {
+            console.log(m);
+            const movieDetail = showMovieDetail(m);
+            $(".modal-body").html(movieDetail);
+          },
+          error: (e) => {
+            console.log(e.responseText);
+          },
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
+});
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+            <div class="card">
+              <img src="${m.Poster}" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">${m.Title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
+                data-bs-target="#movieDetailModal" data-bs-imdbid="${m.imdbID}">Show Details</a>
+              </div>
+            </div>
+          </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fuild">
+            <div class="row">
+              <div class="col-md-3">
+                <img src="${m.Poster}" class="img-fluid" />
+              </div>
+              <div class="col-md">
+                <ul class="list-group">
+                  <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                  <li class="list-group-item">
+                    <strong>Director : </strong> ${m.Director}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Actors  : </strong>${m.Actors}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Writer  : </strong> ${m.Writer}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Plot  : </strong> <br />
+                    ${m.Plot}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>`;
+}
